2d7d675bb6ac834298d2248d5d33801630ac2f67
ruslandavydov <ruslandav@rambler.ru>
2021-02-10 00:29:57 +0300

    0.4. Пользовательские исключения. [#444279] with break

--- src/main/java/ru/job4j/ex/FindEl.java
+++ src/main/java/ru/job4j/ex/FindEl.java
@@ -7,6 +7,7 @@ public class FindEl {
         for (int index = 0; index < value.length; index++) {
             if (value[index].equals(key)) {
                 rsl = index;
+                break;
             }
         }
         if (rsl == -1) {
