174c1c704d983075aa50159c3d37383bb0850141
ruslandavydov <ruslandav@rambler.ru>
2021-02-10 11:37:30 +0300

    1.1. Тесты на StartUI. [#444282]

--- src/test/java/ru/job4j/tracker/StartUITest.java
+++ src/test/java/ru/job4j/tracker/StartUITest.java
@@ -150,4 +150,27 @@ public class StartUITest {
                         + "=== Exit Program ===" + System.lineSeparator()
         ));
     }
+
+    @Test
+    public void whenInvalidExit() {
+        Output out = new StubOutput();
+        Input in = new StubInput(
+                new String[]{"-1", "0"/* Пункты меню: неверный, верный.*/}
+        );
+        Tracker tracker = new Tracker();
+        UserAction[] actions = {
+                new ExitAction(out)
+        };
+        new StartUI(out).init(in, tracker, actions);
+        assertThat(out.toString(), is(
+                String.format(
+                        "Menu%n"
+                                + "0. Exit Program%n"
+                                + "Wrong input, you can select : 0 ... 0%n"
+                                + "Menu%n"
+                                + "0. Exit Program%n"
+                                + "=== Exit Program ===%n"
+                )
+        ));
+    }
 }
