54534934a6a0d98319ca99c0b4d4a8a3f87e2e3f
ruslandavydov <ruslandav@rambler.ru>
2021-02-24 13:58:33 +0300

    4. Сортировка номера [#444231]

--- src/main/java/ru/job4j/collection/LexSort.java
+++ src/main/java/ru/job4j/collection/LexSort.java
@@ -6,8 +6,10 @@ public class LexSort implements Comparator<String>{
 
     @Override
     public int compare(String left, String right) {
+        String[] leftSplit = left.split("\\.");
+        String[] rightSplit = right.split("\\.");
         int result = Integer.compare(left.length(), right.length());
-        int min = Math.min(left.length(), right.length());
+        int min = Math.min(leftSplit.length, rightSplit.length);
         for (int i = 0; i < min; i++) {
             result = Character.compare(left.charAt(i), right.charAt(i));
         }
