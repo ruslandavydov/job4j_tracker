2000196e727b0c1e6b6e7c33240fdec1db6aab61
ruslandavydov <ruslandav@rambler.ru>
2021-02-24 13:26:14 +0300

    4. Сортировка номера [#444231]

---
+++ src/main/java/ru/job4j/collection/LexSort.java
@@ -0,0 +1,16 @@
+package ru.job4j.collection;
+
+import java.util.Comparator;
+
+public class LexSort implements Comparator<String>{
+
+    @Override
+    public int compare(String left, String right) {
+        int result = Integer.compare(left.length(), right.length());
+        int min = Math.min(left.length(), right.length());
+        for (int i = 0; i < min; i++) {
+            result = Character.compare(left.charAt(i), right.charAt(i));
+        }
+        return result;
+    }
+}
