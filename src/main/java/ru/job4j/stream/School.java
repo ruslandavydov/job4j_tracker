6435c6130666b361a3f27fb287d0e034c37d6f60
ruslandavydov <ruslandav@rambler.ru>
2021-03-02 14:08:32 +0300

    1. Фильтрация учеников [#444069]

--- src/main/java/ru/job4j/stream/School.java
+++ src/main/java/ru/job4j/stream/School.java
@@ -7,13 +7,8 @@ import java.util.stream.Collectors;
 public class School {
 
     public List<Student> collect(List<Student> students, Predicate<Student> predict) {
-        List<Student> cls = students.stream()
-                .filter(predict.or(student -> student.getScore() >= 70))
-                .collect(Collectors.toList()).stream()
-                .filter(predict.or(student -> student.getScore() < 70 && student.getScore() >= 50))
-                .collect(Collectors.toList()).stream()
-                .filter(predict.or(student -> student.getScore() < 50))
+        return students.stream()
+                .filter(predict)
                 .collect(Collectors.toList());
-        return cls;
     }
 }
