165f549dead56d6e8bf7819f968059a1ea7aba23
ruslandavydov <ruslandav@rambler.ru>
2021-03-02 04:43:54 +0300

    0. Stream API [#444068]

---
+++ src/main/java/ru/job4j/stream/PositiveNumbers.java
@@ -0,0 +1,21 @@
+package ru.job4j.stream;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class PositiveNumbers {
+
+    public static void main(String[] args) {
+        List<Integer> numbs = new ArrayList<>();
+        numbs.add(1);
+        numbs.add(-2);
+        numbs.add(3);
+        numbs.add(-4);
+        numbs.add(-5);
+        List<Integer> posNumbs = numbs.stream().filter(
+                numb -> numb > 0
+        ).collect(Collectors.toList());
+        System.out.println(posNumbs);
+    }
+}
