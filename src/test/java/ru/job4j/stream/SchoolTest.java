c8f46fccbf3e85b37e5fe52efe3a773f0d63cd43
ruslandavydov <ruslandav@rambler.ru>
2021-03-03 13:00:40 +0300

    4. Преобразование List в Map. [#444072]

--- src/test/java/ru/job4j/stream/SchoolTest.java
+++ src/test/java/ru/job4j/stream/SchoolTest.java
@@ -3,8 +3,7 @@ package ru.job4j.stream;
 import org.junit.Before;
 import org.junit.Test;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 import java.util.function.Predicate;
 
 import static org.hamcrest.Matchers.is;
@@ -59,6 +58,23 @@ public class SchoolTest {
         expected.add(new Student(20, "Surname2"));
         expected.add(new Student(30, "Surname3"));
         expected.add(new Student(40, "Surname4"));
+        assertThat(rsl, is(expected));
+    }
+
+    @Test
+    public void map() {
+        School sc = new School();
+        Map<String, Student> rsl = sc.map(students);
+        Map<String, Student> expected = new HashMap<>();
+        expected.put("Surname9", new Student(90, "Surname9"));
+        expected.put("Surname8", new Student(80, "Surname8"));
+        expected.put("Surname7", new Student(70, "Surname7"));
+        expected.put("Surname6", new Student(60, "Surname6"));
+        expected.put("Surname5", new Student(50, "Surname5"));
+        expected.put("Surname4", new Student(40, "Surname4"));
+        expected.put("Surname3", new Student(30, "Surname3"));
+        expected.put("Surname2", new Student(20, "Surname2"));
+        expected.put("Surname1", new Student(10, "Surname1"));
         assertThat(rsl, is(expected));
     }
 }
